1f5d4c94b806141b19a0fa2b8fc986224daedbaf
Robert O'Rourke <robert@o-rourke.org>
2016-04-27 12:57:03 -0400

    sets jpegs to be twice the dimensions but 40% quality, preserves more detail

--- index.js
+++ index.js
@@ -19,19 +19,26 @@ module.exports = function( region, bucket, key, args, callback ) {
 
 		try {
 			var image = sharp( data.Body ).withMetadata()
+			var sizeModifier = 1;
 
 			// convert gifs to pngs
-			if( path.extname( key ).toLowerCase() === '.gif' ) {
+			if ( path.extname( key ).toLowerCase() === '.gif' ) {
 				image.png()
 			}
 
+			// set jpg quality and sizeModifier
+			if ( path.extname( key ).toLowerCase().match(/jpe?g/) ) {
+				sizeModifier = 2
+				image.quality(40)
+			}
+
 			if ( args.resize ) {
-				image.resize.apply( image, args.resize.split(',').map( function( v ) { return Number( v ) } ) )
+				image.resize.apply( image, args.resize.split(',').map( function( v ) { return Number( v ) * sizeModifier } ) )
 			} else if ( args.fit ) {
-				image.resize.apply( image, args.fit.split(',').map( function( v ) { return Number( v ) } ) )
+				image.resize.apply( image, args.fit.split(',').map( function( v ) { return Number( v ) * sizeModifier } ) )
 				image.max()
 			} else if ( args.w || args.h ) {
-				image.resize( Number( args.w ), Number( args.h ) )
+				image.resize( Number( args.w ), Number( args.h ) * sizeModifier )
 
 				if ( ! args.crop ) {
 					image.max()
