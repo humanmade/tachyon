3ee9326a11a25a896597b1bf86297267541b786e
Joe Hoyle <joehoyle@gmail.com>
2017-04-21 10:12:05 +0800

    Tabs not spaces, long live the TAB

--- lambda-handler.js
+++ lambda-handler.js
@@ -1,31 +1,30 @@
 var tachyon = require('./index');
 
 exports.handler = function(event, context, callback) {
-  var region = process.env.S3_REGION;
-  var bucket = process.env.S3_BUCKET;
-  var key = event.path.substring(1);
-  var args = event.queryStringParameters;
-  return tachyon.s3(
-    { region: region, bucket: bucket },
-    key,
-    args,
-    function(err, data, info) {
-      if (err) {
-        return context.fail(err);
-      }
-      var resp = {
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'image/' + info.format,
-        },
-        body: new Buffer(data).toString('base64'),
-        isBase64Encoded: true,
-      };
-      callback(null, resp);
+	var region = process.env.S3_REGION;
+	var bucket = process.env.S3_BUCKET;
+	var key = event.path.substring(1);
+	var args = event.queryStringParameters;
+	return tachyon.s3({ region: region, bucket: bucket }, key, args, function(
+		err,
+		data,
+		info
+	) {
+		if (err) {
+			return context.fail(err);
+		}
+		var resp = {
+			statusCode: 200,
+			headers: {
+				'Content-Type': 'image/' + info.format,
+			},
+			body: new Buffer(data).toString('base64'),
+			isBase64Encoded: true,
+		};
+		callback(null, resp);
 
-      data = null;
-      info = null;
-      err = null;
-    }
-  );
+		data = null;
+		info = null;
+		err = null;
+	});
 };
