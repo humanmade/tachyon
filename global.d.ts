19b3ef94923929e6c034259d2f5dd13509d26919
Joe Hoyle <joehoyle@gmail.com>
2023-08-09 15:28:53 +0200

    Add lint npm command

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,5 @@
 declare var awslambda: {
 	streamifyResponse: (
-		handler: (event: APIGatewayProxyEventV2, response: ResponseStream) => Promise<any>
-	) => (event: APIGatewayProxyEventV2, context: ResponseStream) => void;
+		handler: ( event: APIGatewayProxyEventV2, response: ResponseStream ) => Promise<any>
+	) => ( event: APIGatewayProxyEventV2, context: ResponseStream ) => void;
 };
