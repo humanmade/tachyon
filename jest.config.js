8c913ac7751043781640509670ab72c1b7edb4f9
Joe Hoyle <joehoyle@gmail.com>
2023-11-20 20:58:15 +0100

    Add Jest tests
    This moves the tests into `./tests`, with filesize and animated files tests. I've updated the contributing docs too.

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	testMatch: ['<rootDir>/tests/**/test-*.ts'],
+	extensionsToTreatAsEsm: ['.ts'],
+	transform: {
+		'^.+\\.tsx?$': [
+			'ts-jest',
+			{
+				useESM: true,
+				tsconfig: './tsconfig.test.json',
+			},
+		],
+	},
+};
--- src/lib.ts
+++ src/lib.ts
@@ -4,9 +4,6 @@ import sharp from 'sharp';
 import smartcrop from 'smartcrop-sharp';
 
 export interface Args {
-	// Required args.
-	key: string;
-
 	// Optional args.
 	background?: string;
 	crop?: string | string[];
