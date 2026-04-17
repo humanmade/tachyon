d9416e5e320fb9e86e70d626f4b02a60234c5e39
Wisdom Hambolu <wisyhambolu@gmail.com>
2026-04-17 17:03:54 +0100

    Set setupFilesAfterEnv in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ export default {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	testMatch: ['<rootDir>/tests/**/test-*.ts'],
-	setupFiles: ['<rootDir>/tests/setup.ts'],
+	setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
 	extensionsToTreatAsEsm: ['.ts'],
 	transform: {
 		'^.+\\.tsx?$': [
